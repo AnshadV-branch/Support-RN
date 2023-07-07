da2178cc817ea2f92b45d1aa60cdd463889c0c1c
pawanpal-branch <pawan.pal@branch.io>
2023-07-07 09:08:35 +0530

    added testcase in ios and android code, fix iOS build error

--- android/app/src/main/java/com/supporttest/MainApplication.java
+++ android/app/src/main/java/com/supporttest/MainApplication.java
@@ -55,8 +55,10 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
 
-      //RNBranchModule.enableLogging();
+      RNBranchModule.enableLogging();
       RNBranchModule.getAutoInstance(this);
+      RNBranchModule.setDebug();
+
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
--- ios/SupportTest/AppDelegate.mm
+++ ios/SupportTest/AppDelegate.mm
@@ -12,7 +12,10 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+   [RNBranch enableLogging];
+   [RNBranch useTestInstance];
    [RNBranch initSessionWithLaunchOptions:launchOptions isReferrable:YES];
+
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
